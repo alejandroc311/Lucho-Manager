f0d6caadc92b4b79c5b9348d9206dfa5738f0348
alejandroc311 <alejandroc311@gmail.com>
2016-06-17 14:11:36 -0400

    Trials for implementing a pool of connections instead of a single connection

--- src/Model/DBConnector.java
+++ src/Model/DBConnector.java
@@ -1,12 +1,17 @@
 package Model;
 
+import sun.jdbc.odbc.ee.ConnectionPoolDataSource;
 import sun.rmi.runtime.Log;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import javax.sql.PooledConnection;
 import java.io.Console;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 /**
  * Created by Alejandro on 6/10/2016.
@@ -16,8 +21,20 @@ public class DBConnector {
     private String url = "jdbc:mysql://localhost:3306/javabase?&useSSL=false";
     private String username = "java";
     private String password = "Gohanssj2";
+    private javax.sql.ConnectionPoolDataSource mConnectionPoolDataSource;
+    private DataSource mDataSource;
+    private InitialContext mInitialContext;
     private Connection mConnection = null;
     public DBConnector(){}
+
+    public void setConnectionPool(){}
+
+    public InitialContext getContext(){
+        Properties contextProperties = new Properties();
+        contextProperties.put(Context.INITIAL_CONTEXT_FACTORY,"");
+
+        return null;
+    }
 
     public void setConnection(){
         try{
